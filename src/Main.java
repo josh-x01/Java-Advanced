cc23d05a6dfe5c149edc3958cb1116b1f70e8852
eyasuyid <eyasuyid@gmail.com>
2022-11-16 19:05:33 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import generic.GenericFunc.*;
+import static generic.GenericFunc.*;
+
+public class Main {
+    public static void main(String[] args) {
+    	System.out.println("Hello linux in java");
+        testGeneric();
+    }
+}
